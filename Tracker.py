f59372f9901f33a996136b87b3e3e7a4b2f44810
AnthonyTranCode <anthonytran_@live.ca>
2020-12-20 23:00:24 -0700

    Dynamic working timer

--- Tracker.py
+++ Tracker.py
@@ -6,24 +6,24 @@ import time
 
 ##User Interface
 root = tk.Tk()
-canvas = tk.Canvas(root, height=100, width=300)
+canvas = tk.Canvas(root, height=200, width=300)
 canvas.pack()
 
 frame = tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
-activityNum = "None"
 
 ## Manually input of activity code
 def button_add():
     input_activity_code = activity_input.get()
-    activityNum.config(text="Activity Code: " + input_activity_code)
-    
+    newButton = tk.Button(frame, text = input_activity_code, padx=10, pady=5, fg="white", bg = "#263D42")
+    newButton.bind('<Button-1>', lambda event : toggle_timer(event, input_activity_code))
+    newButton.pack()
+
+    activity_state_dict[input_activity_code] = "inactive"
+    activity_session_start_time_dict[input_activity_code] = 0.00
+    activity_time_elapsed_dict[input_activity_code] = 0.00
     return None 
 
-
-activity_inputs ={
-
-}
 
 activity_input = tk.Entry(frame, fg="white", bg = "grey")
 activity_input.pack()
@@ -31,50 +31,39 @@ activity_input.pack()
 new_activity= tk.Button(frame, text = "Add new activity code",  padx=10, pady=5, fg="white", bg = "#263D42", command = button_add)
 new_activity.pack()
 
-activityNum = tk.Button(frame, text = "", padx=10, pady=5, fg="white", bg = "#263D42")
-activityNum.pack()
 
 
 ## To track the state of each button (possible state is inactive or active)
-activity_state_dict = {
-    activityNum : "inactive"
-}
+activity_state_dict = {}
+
 ## To track the cumulative time that has elapsed for each activity code
-activity_time_elapsed_dict = {
-    activityNum : 0.00
-}
+activity_time_elapsed_dict = {}
 
 ## To track the start time of the active activity session
-activity_session_start_time_dict = {
-    activityNum : 0.00
-}
+activity_session_start_time_dict = {}
+
 
 ## Toggle Function
-def toggle_timer(event):
-    if activity_state_dict[activityNum] == "inactive":
-        activity_state_dict[activityNum] = "active"
-        print(activity_state_dict[activityNum])
-        start_timer()
-    elif activity_state_dict[activityNum] == "active":
-        activity_state_dict[activityNum] = "inactive"
-        print(activity_state_dict[activityNum])
-        elapsed_timer()
-        print(activity_time_elapsed_dict[activityNum])
+def toggle_timer(event, activity_code):
+    if activity_state_dict[activity_code] == "inactive":
+        activity_state_dict[activity_code] = "active"
+        print(activity_state_dict[activity_code])
+        start_timer(activity_code)
+    elif activity_state_dict[activity_code] == "active":
+        activity_state_dict[activity_code] = "inactive"
+        print(activity_state_dict[activity_code])
+        elapsed_timer(activity_code)
+        print(activity_time_elapsed_dict[activity_code])
 
 
     
-def start_timer():
+def start_timer(activity_code):
     startTime = time.time() 
-    activity_session_start_time_dict[activityNum] = startTime
-def elapsed_timer():
+    activity_session_start_time_dict[activity_code] = startTime
+def elapsed_timer(activity_code):
     stopTime = time.time()
-    elapsedTime = stopTime - activity_session_start_time_dict[activityNum]
-    activity_time_elapsed_dict[activityNum] = round((activity_time_elapsed_dict[activityNum] + elapsedTime), 2)
-
-
-activityNum.bind('<Button-1>', toggle_timer)
-
-
+    elapsedTime = stopTime - activity_session_start_time_dict[activity_code]
+    activity_time_elapsed_dict[activity_code] = round((activity_time_elapsed_dict[activity_code] + elapsedTime), 2)
 
 
 root.mainloop()
