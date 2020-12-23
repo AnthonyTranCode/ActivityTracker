5b3055376fed0b01865095f4ca5845c17568e262
AnthonyTranCode <anthonytran_@live.ca>
2020-12-23 00:50:48 -0700

    Working update

--- Tracker.py
+++ Tracker.py
@@ -7,23 +7,23 @@ import time
 root = tk.Tk()
 canvas = tk.Canvas(root, height=200, width=300)
 canvas.pack()
+root.title("Activity Tracker")
 
 frame = tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-
 def button_add():
     input_activity_code = activity_input.get()
-    newButton = tk.Button(frame, text = input_activity_code, padx=10, pady=5, fg="white", bg = "#263D42")
+    newButton = tk.Button(frame, text = "Activity Code: " + input_activity_code, padx=10, pady=5, fg="white", bg = "#263D42")
     newButton.bind('<Button-1>', lambda event : toggle_timer(event, input_activity_code))
-    newButton.pack()
+    newButton.pack() 
     
     ## initialize dictionary for initial values
+
     activity_state_dict[input_activity_code] = "inactive"
     activity_session_start_time_dict[input_activity_code] = 0.00
     activity_time_elapsed_dict[input_activity_code] = 0.00
-    return None 
-
+    activity_button_dict[input_activity_code] = newButton
 
 ## Manual input of activity code
 activity_input = tk.Entry(frame, fg="white", bg = "grey")
@@ -42,16 +42,39 @@ activity_session_start_time_dict = {}
 ## To track the cumulative time that has elapsed for each activity code
 activity_time_elapsed_dict = {}
 
+## To track the activity code and button 
+activity_button_dict = {}
+
+active_button_activity_code = ""
 
 ## Toggle function for each button
 def toggle_timer(event, activity_code):
+    global active_button_activity_code
+
+    ## If we select a inactive button
     if activity_state_dict[activity_code] == "inactive":
         activity_state_dict[activity_code] = "active"
+
+        ## Stop previous timer & unselecting active button
         print(activity_state_dict[activity_code])
+        if active_button_activity_code != "":
+            activity_state_dict[active_button_activity_code] = "inactive"
+            activity_button_dict[active_button_activity_code].config(bg =  "#263D42")
+            elapsed_timer(active_button_activity_code)
+
+        ## Set the new active button & start timer
+        active_button_activity_code = activity_code
+        activity_button_dict[activity_code].config(bg = "red")
         start_timer(activity_code)
+    
+    ## If we click an active button
     elif activity_state_dict[activity_code] == "active":
         activity_state_dict[activity_code] = "inactive"
         print(activity_state_dict[activity_code])
+        
+        ## Turning inactive button off 
+        activity_button_dict[activity_code].config(bg =  "#263D42")
+        active_button_activity_code = ""
         elapsed_timer(activity_code)
         print(activity_time_elapsed_dict[activity_code])
 
@@ -64,6 +87,5 @@ def elapsed_timer(activity_code):
     stopTime = time.time()
     elapsedTime = stopTime - activity_session_start_time_dict[activity_code]
     activity_time_elapsed_dict[activity_code] = round((activity_time_elapsed_dict[activity_code] + elapsedTime), 2)
-
 
 root.mainloop()
